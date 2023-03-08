1f8adc95e9b8b70cd69592a10eddd137e7294bed
HoangJerry304 <hoangninh3004@gmail.com>
2023-03-09 00:28:39 +0700

    cs

--- furama-resort-angular/src/app/app.module.ts
+++ furama-resort-angular/src/app/app.module.ts
@@ -2,10 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { NavComponent } from './component/nav/nav.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { CarouselComponent } from './component/carousel/carousel.component';
+import { RoomCardComponent } from './component/room-card/room-card.component';
+import { FacilityComponent } from './component/facility/facility.component';
+import { CustomerComponent } from './component/customer/customer.component';
+import { ContractComponent } from './component/contract/contract.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavComponent,
+    FooterComponent,
+    CarouselComponent,
+    RoomCardComponent,
+    FacilityComponent,
+    CustomerComponent,
+    ContractComponent
   ],
   imports: [
     BrowserModule
