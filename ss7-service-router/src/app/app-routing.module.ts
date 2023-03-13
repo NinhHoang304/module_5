e428e47c312094917f35f002f6c0667fdee1e651
HoangJerry304 <hoangninh3004@gmail.com>
2023-03-13 13:40:40 +0700

    commit

---
+++ ss7-service-router/src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import {TimelinesComponent} from './timelines/timelines.component';
+import {YoutubePlaylistComponent} from './youtube-playlist/youtube-playlist.component';
+
+
+const routes: Routes = [
+  {
+    path: 'timelines',
+    component: TimelinesComponent
+  },
+  {
+    path: 'youtube',
+    component: YoutubePlaylistComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
